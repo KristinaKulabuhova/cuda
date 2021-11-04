2c1c063069fef208805c60119bb3193cb707ed0c
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2021-11-04 18:39:52 +0300

    Mul

--- runners/02-mul.cu
+++ runners/02-mul.cu
@@ -1,3 +1,49 @@
-int main() {
+#include <cstdint>
+#include <cassert>
+#include <iostream>
 
+using std::cout;
+using std::endl;
+
+#include "KernelMul.cuh"
+
+
+int main() {
+  const int N = (1 << 28);
+  const int block_size = 256;
+  const int n_blocks = (N + block_size - 1) / block_size;
+  float *x, *y, *mul;
+  const uint64_t array_byte_len = N * sizeof(*x);
+  float time;
+
+  cudaMallocManaged(&x, array_byte_len);
+  cudaMallocManaged(&y, array_byte_len);
+  cudaMallocManaged(&mul, array_byte_len);
+  for (int i = 0; i < N; ++i) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+  cudaEvent_t start;
+  cudaEvent_t stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start);
+
+  KernelMul<<<n_blocks, block_size>>>(N, x, y, mul);
+
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+	cudaDeviceSynchronize();
+  cudaEventElapsedTime(&time, start, stop);
+  std::cout << time << std::endl;
+
+  for(int i = 0; i < N; ++i) {
+    assert(mul[i] == 2.0f);
+  }
+
+  cudaFree(x);
+  cudaFree(y);
+  cudaFree(mul);
+
+  return 0;
 }
--- src/KernelMul.cu
+++ src/KernelMul.cu
@@ -1,6 +1,11 @@
 #include <KernelMul.cuh>
 
 __global__ void KernelMul(int numElements, float* x, float* y, float* result) {
-  // your code here
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  
+  for (int i = idx; i < numElements; i += stride) {
+      result[i] = x[i] + y[i];
+  }
 }
 
