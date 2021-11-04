89804e531a9c27838850bedc409913b5b82c3940
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2021-11-04 18:58:01 +0300

    MatrixAdd

--- runners/03-matrix-add.cu
+++ runners/03-matrix-add.cu
@@ -1,7 +1,53 @@
+#include <assert.h>
+#include <iostream>
+
 #include <KernelMatrixAdd.cuh>
 
 int main() {
-  // YOUR CODE HERE
-  // you must use pitched mechanism to pass code review
-}
+  float *d_A, *d_B, *d_C;
+  size_t pitch;
+  int W = 10000;
+  int H = 10000;
 
+  dim3 block_size{32, 32}; // max threads in block 1024
+  dim3 n_blocks{(W + block_size.x - 1) / block_size.x, (H + block_size.y - 1) / block_size.y};
+
+  float *h_A = new float[W * H];
+  float *h_B = new float[W * H];
+  float *h_C = new float[W * H];
+
+  for(int row = 0; row < H; ++row) {
+    for(int col = 0; col < W; ++col) {
+      d_A[row * W + col] = 1.0f;
+      d_B[row * W + col] = 2.0f;
+    }
+  }
+
+  cudaMallocPitch(&d_A, &pitch, W * sizeof(float), H);
+  cudaMallocPitch(&d_B, &pitch, W * sizeof(float), H);
+  cudaMallocPitch(&d_C, &pitch, W * sizeof(float), H);
+
+  cudaMemcpy2D(d_A, pitch, h_A, W * sizeof(float), W * sizeof(float), H, cudaMemcpyHostToDevice);
+  cudaMemcpy2D(d_B, pitch, h_B, W * sizeof(float), W * sizeof(float), H, cudaMemcpyHostToDevice);
+
+  KernelMatrixAdd<<<n_blocks, block_size>>>(H, W, pitch, A, B, C);
+	cudaDeviceSynchronize();
+
+  cudaMemcpy2D(h_C, W * sizeof(float), d_C, pitch, W * sizeof(float), H, cudaMemcpyDeviceToHost));
+
+  for (int row = 0; row < H; ++row) {
+    for(int col = 0; col < W; ++col) {
+      assert(h_C[row * W + col] == 3.0f);
+    }
+  }
+
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+
+  delete[] h_A;
+  delete[] h_B;
+  delete[] h_C;
+
+  return 0;
+}
--- src/KernelMatrixAdd.cu
+++ src/KernelMatrixAdd.cu
@@ -1,6 +1,20 @@
 #include <KernelMatrixAdd.cuh>
 
 __global__ void KernelMatrixAdd(int height, int width, int pitch, float* A, float* B, float* result) {
-  // Your code here
+  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  const int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	const int stride_x = blockDim.x * gridDim.x;
+	const int stride_y = blockDim.y * gridDim.y;
+
+  for (int row = y; row < height; row += stride_y) {
+    float* A_y = reinterpret_cast<float*>(reinterpret_cast<char*>(A) + row * pitch);
+    float* B_y = reinterpret_cast<float*>(reinterpret_cast<char*>(B) + row * pitch);
+    float* res_y = reinterpret_cast<float*>(reinterpret_cast<char*>(result) + row * pitch);
+
+    for(int col = x; col < width; col += stride_col) {
+      res_y[col] = A_y[col] + B_y[col];
+    }
+	}
 }
 
